179b084e40f3e3bb05c671df61d96bbcc1644eaa
Guillaume Lebegue <guillaume.lebegue@epitech.eu>
2023-07-24 23:58:49 +0200

    :sparkles: : Add Basic paper plugin

---
+++ src/main/java/fr/araxgaming/deathpenalty/DeathPenaltyPlugin.java
@@ -0,0 +1,26 @@
+package fr.araxgaming.deathpenalty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathPenaltyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+    }
+}
