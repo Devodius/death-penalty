a00a01036db57805a2c6f6396a4722170f3493c7
HyroxTek <72016402+HyroxTek@users.noreply.github.com>
2024-05-19 16:48:49 +0200

    Fix TotemOfKeepInventoryRecipe Shaped Recipe import

--- src/main/java/fr/araxgaming/deathpenalty/recipes/totems/TotemOfKeepInventoryRecipes.java
+++ src/main/java/fr/araxgaming/deathpenalty/recipes/totems/TotemOfKeepInventoryRecipes.java
@@ -8,7 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
-import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
